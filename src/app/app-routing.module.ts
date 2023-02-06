683fa59763261436ff0826ac470c8d353eb91e68
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-02-06 19:28:28 +0100

    routes completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { HomeComponent } from './pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', component: HomeComponent
+  },
+  {
+    path:'gallery', component: GalleryComponent
+  },
+  {
+    path:'about', component: AboutComponent
+  },
+  {
+    path:'contact', component: ContactComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { GalleryComponent } from './pages/gallery/gallery.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { NavigatorComponent } from './shared/components/navigator/navigator.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { ContactComponent } from './pages/contact/contact.component';
     HomeComponent,
     AboutComponent,
     GalleryComponent,
-    ContactComponent
+    ContactComponent,
+    FooterComponent,
+    NavigatorComponent
   ],
   imports: [
     BrowserModule,
